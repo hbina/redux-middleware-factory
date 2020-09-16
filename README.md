3e60b91c145cdf2fd9914749c17d0c79c7c8920f
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-09-17 06:36:10 +0800

    Now able to support async middleware with a bit of a hack
    If user wants to implement an async middleware that recognize a function,
    simply modify the type of A to be a `any`. It should be possible to narrow it down to just
    a function F but Idk enough TS to do it.

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Helper to generate `Redux`'s middlewares.
 
 # TODO
 
-1.  This library currently does not support async middleware :(
-    This is due to the fact that `MiddlewareAPI` expects D to be `Dispatch<AnyAction>`.
+1.  ~~This library currently does not support async middleware :(
+    This is due to the fact that `MiddlewareAPI` expects D to be `Dispatch<AnyAction>`.~~
--- test/redux-middleware-factory.test.ts
+++ test/redux-middleware-factory.test.ts
@@ -1,4 +1,11 @@
-import { AnyAction, applyMiddleware, createStore } from '@reduxjs/toolkit'
+import {
+  Action,
+  AnyAction,
+  applyMiddleware,
+  createStore,
+  Reducer,
+  ThunkDispatch
+} from '@reduxjs/toolkit'
 
 import { reduxMiddlewareFactory } from '../src/redux-middleware-factory'
 
@@ -18,7 +25,7 @@ enum ActionType {
 
 type TestAction = { type: ActionType }
 
-export const reducer = (state = defaultState, action: TestAction) => {
+export const reducer: Reducer<TestState, any> = (state = defaultState, action) => {
   switch (action.type) {
     case ActionType.SET_ONE: {
       return { value: 1 }
@@ -34,7 +41,8 @@ export const reducer = (state = defaultState, action: TestAction) => {
   }
 }
 
-const mappingMiddleware = reduxMiddlewareFactory<TestState, any>((_store, next, action) => {
+// A middleware that simply maps `ActionType.SET_ONE` to `ActionType.SET_THREE`.
+const mappingMiddleware = reduxMiddlewareFactory<TestState>((_store, next, action) => {
   if (action.type === ActionType.SET_ONE) {
     return next({ type: ActionType.SET_THREE })
   } else {
@@ -53,6 +61,52 @@ describe('Maps an action of certain type to another type of action.', () => {
     store.dispatch({ type: ActionType.SET_TWO })
     expect(store.getState().value).toEqual(2)
     store.dispatch({ type: ActionType.SET_ONE })
+    expect(store.getState().value).toEqual(3)
+  })
+})
+
+// If we want to have an async middleware, we have to loosen up the type here.
+// Namely, `action` must be `any`.
+const asyncMiddleware = reduxMiddlewareFactory<TestState, any>((store, next, action) => {
+  if (typeof action === 'function') {
+    action(store.dispatch, action)
+    return action
+  }
+  return next(action)
+})
+
+export const asyncAction: (
+  a: ActionType
+) => (b: ThunkDispatch<TestState, undefined, Action>) => Promise<void> = type => {
+  return async dispatch => {
+    switch (type) {
+      case ActionType.SET_ONE: {
+        dispatch({ type: ActionType.SET_ONE })
+        break
+      }
+      case ActionType.SET_TWO: {
+        dispatch({ type: ActionType.SET_TWO })
+        break
+      }
+      case ActionType.SET_THREE: {
+        dispatch({ type: ActionType.SET_THREE })
+        break
+      }
+    }
+  }
+}
+
+describe('Execute a thunk.', () => {
+  const store = createStore(reducer, applyMiddleware(asyncMiddleware, mappingMiddleware))
+
+  it('Execute a bunch of thunks', () => {
+    store.dispatch(asyncAction(ActionType.SET_TWO))
+    expect(store.getState().value).toEqual(2)
+    store.dispatch(asyncAction(ActionType.SET_THREE))
+    expect(store.getState().value).toEqual(3)
+    store.dispatch(asyncAction(ActionType.SET_TWO))
+    expect(store.getState().value).toEqual(2)
+    store.dispatch(asyncAction(ActionType.SET_ONE))
     expect(store.getState().value).toEqual(3)
   })
 })
